abf6f348aaa186053f7f8c99c6ba386426622277
sashi0034 <satoshi3104.nishi@gmail.com>
2022-10-01 10:22:02 +0900

    blobwobを動かし処理を追加

--- hello-seed/src/Main.hs
+++ hello-seed/src/Main.hs
@@ -12,15 +12,16 @@ import qualified SDLWrapper
 import Control.Monad (unless)
 import Control.Monad.IO.Class (MonadIO)
 import Data.Foldable   (foldl')
+import Data.StateVar
 
 
 data World = World
   { exiting :: Bool
   , frame   :: Int
+  , playerPos :: Pos
   }
 
 
-data Intent = Idle | Quit
 
 
 data Size = Size
@@ -28,11 +29,25 @@ data Size = Size
   , height :: Int
   }
 
+data Pos = Pos
+  { x :: Int
+  , y :: Int
+  }
+
+data InputIntent
+  = Idle
+  | Left
+  | Right
+  | Up
+  | Down
+  | Quit
+
 
 initialApp :: World
 initialApp = World
   { exiting = False
   , frame   = 0
+  , playerPos = Pos 0 0
   }
 
 
@@ -61,24 +76,45 @@ appLoop r a
   >>= \a' -> a' <$ r a'
 
 
-updateApp :: World -> [Intent] -> World
+updateApp :: World -> [InputIntent] -> World
 updateApp a = stepFrame . foldl' applyIntent a
 
 
-pollIntents :: (MonadIO m) => m [Intent]
+pollIntents :: (MonadIO m) => m [InputIntent]
 pollIntents = eventToIntent `fmap2` SDL.pollEvents
 
   where
     fmap2 = fmap . fmap
 
 
-eventToIntent :: SDL.Event -> Intent
-eventToIntent (SDL.Event _t SDL.QuitEvent) = Quit
-eventToIntent _                            = Idle
+eventToIntent :: SDL.Event -> InputIntent
+eventToIntent (SDL.Event _t SDL.QuitEvent) = Main.Quit
+eventToIntent (SDL.Event _t (SDL.KeyboardEvent k)) = keyEventToIntent k
+eventToIntent _                            = Main.Idle
 
 
-applyIntent :: World -> Intent -> World
+keyEventToIntent :: SDL.KeyboardEventData -> InputIntent
+keyEventToIntent (SDL.KeyboardEventData _ SDL.Pressed _ keysym) =
+  case SDL.keysymKeycode keysym of
+
+    SDL.KeycodeEscape -> Quit
+
+    SDL.KeycodeA -> Main.Left
+    SDL.KeycodeD -> Main.Right
+    SDL.KeycodeW -> Main.Up
+    SDL.KeycodeS -> Main.Down
+
+    _            -> Idle
+
+
+keyEventToIntent _ = Idle
+
+applyIntent :: World -> InputIntent -> World
 applyIntent a Quit = a { exiting = True }
+applyIntent a Main.Left = a { playerPos = Pos (-2 + (x $ playerPos a)) (y $ playerPos a) }
+applyIntent a Main.Right = a { playerPos = Pos (2 + (x $ playerPos a)) (y $ playerPos a) }
+applyIntent a Main.Up = a { playerPos = Pos (x $ playerPos a) (-2 + (y $ playerPos a)) }
+applyIntent a Main.Down = a { playerPos = Pos (x $ playerPos a) (2 + (y $ playerPos a)) }
 applyIntent a Idle = a
 
 
@@ -88,8 +124,11 @@ stepFrame a = a { frame = frame a + 1 }
 
 renderApp :: (MonadIO m) => SDL.Renderer -> SDL.Texture -> World -> m ()
 renderApp r t a = do
+  let renderColor = SDL.rendererDrawColor r
+  renderColor $= (SDL.V4 100 100 100 255)
+  
   SDL.clear r
-  SDL.copy r t (Just mask) (Just pos)
+  SDL.copy r t (Just mask) (Just dest)
   SDL.present r
 
   where
@@ -97,17 +136,17 @@ renderApp r t a = do
     numFrame = 10
     cellSize = Size 24 24
     cellScale = 3
-    x = (frame a `div` frameDuration) `mod` numFrame
-    mask = fromIntegral <$> SDLWrapper.mkRect (x * width cellSize) 0 (width cellSize) (height cellSize)
+    srcX = (frame a `div` frameDuration) `mod` numFrame
+    mask = fromIntegral <$> SDLWrapper.mkRect (srcX * width cellSize) 0 (width cellSize) (height cellSize)
 
-    renderingSize = SDLWrapper.mkRect 0 0 (fromIntegral $ width cellSize * cellScale) (fromIntegral $ height cellSize * cellScale)
-    windowSize = SDLWrapper.mkRect 0 0 640 480
-    pos = floor <$> centerWithin renderingSize windowSize
+    --renderingSize = SDLWrapper.mkRect 0 0 (fromIntegral $ width cellSize * cellScale) (fromIntegral $ height cellSize * cellScale)
+    --windowSize = SDLWrapper.mkRect 0 0 640 480
+    dest = fromIntegral <$> SDLWrapper.mkRect (x $ playerPos a) (y $ playerPos a) (cellScale * width cellSize) (cellScale * height cellSize)
 
 
-centerWithin :: (Fractional a) => SDL.Rectangle a -> SDL.Rectangle a -> SDL.Rectangle a
-centerWithin (SDL.Rectangle _ iz) (SDL.Rectangle (SDL.P op) oz)
-  = SDL.Rectangle p iz
+-- centerWithin :: (Fractional a) => SDL.Rectangle a -> SDL.Rectangle a -> SDL.Rectangle a
+-- centerWithin (SDL.Rectangle _ iz) (SDL.Rectangle (SDL.P op) oz)
+--   = SDL.Rectangle p iz
 
-  where
-    p = SDL.P $ op + (oz - iz) / 2
+--   where
+--     p = SDL.P $ op + (oz - iz) / 2
