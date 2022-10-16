0d7b1423637bd93cd51e62dce51864bbeeed416b
sashi0034 <satoshi3104.nishi@gmail.com>
2022-10-17 00:13:46 +0900

    背景をうねうねさせる処理を追加

--- hello-seed/src/MainScene/BackgroundBehavior.hs
+++ hello-seed/src/MainScene/BackgroundBehavior.hs
@@ -1,9 +1,9 @@
 
-module MainScene.BackgroundBehavior 
+module MainScene.BackgroundBehavior
 ( updateBackground
 , renderBackground
 ) where
-  
+
 import Control.Monad.IO.Class
 import qualified SDL
 import ImageRsc
@@ -12,26 +12,45 @@ import qualified SDLWrapper
 import Vec
 import MainScene.MainScene
 import MainScene.Background
+import SDL.Video
 
 
 
 updateBackground :: MonadIO m => MainScene -> m Background
-updateBackground world = do
+updateBackground scene = do
   return background' { animCount = animCount background' + 1 }
-  where 
-    background' = background world
+  where
+    background' = background scene
 
 
 
 renderBackground :: (MonadIO m) => SDL.Renderer -> ImageRsc -> World -> m ()
 renderBackground r imageRsc world = do
-  liftIO $ print $ animCount $ background $ scene world
-  SDL.copy r (blue_bg imageRsc) (Nothing) (Just dest)
+  --liftIO $ print $ animCount $ background $ scene world
 
+  let bgTexture = blue_bg imageRsc
+  bgTextureInfo <- queryTexture bgTexture
+  
+  let bgW = textureWidth bgTextureInfo
+  let bgH = textureHeight bgTextureInfo
+
+  let applySrcPt value = fromIntegral $ floor $ maxAmp + value
+  let srcX1 = applySrcPt currAmp
+  let srcY1 = applySrcPt currAmp
+  let srcX2 = applySrcPt $ -currAmp + fromIntegral bgW
+  let srcY2 = applySrcPt $ -currAmp + fromIntegral bgH
+
+  let src = SDLWrapper.makeRect srcX1 srcY1 srcX2 srcY2
+
+  SDL.copy r (blue_bg imageRsc) (Just src) (Just dest)
 
   where
     dest = SDLWrapper.makeRect 0 0 (fromIntegral $ getX size) (fromIntegral $ getY size)
-    size = windowSize world
-    
+    size = World.windowSize world
+
+    currPhase = (fromIntegral (animCount $ background $ scene world) / 180) * pi :: Float
+    maxAmp = 120 :: Float
+    currAmp = maxAmp * sin currPhase
+
 
 
