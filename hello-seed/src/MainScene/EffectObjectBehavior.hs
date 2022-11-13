f683ac59ca9bd5fd97803e12612e7d8684c461c5
sashi0034 <satoshi3104.nishi@gmail.com>
2022-11-13 21:37:56 +0900

    エフェクトの改良

--- hello-seed/src/MainScene/EffectObjectBehavior.hs
+++ hello-seed/src/MainScene/EffectObjectBehavior.hs
@@ -6,17 +6,20 @@ import Control.Monad.Cont
 import ImageRsc (ImageRenderer (ImageRenderer), ImageRsc (oval_16x16))
 import Vec
 import Rendering
-import MainScene.MainScene (MainScene (effectObjects))
+import MainScene.MainScene
+import MainScene.HarvestManager
 
 
 refreshEffectObjects :: (MonadIO m) => World -> m [EffectObject]
 refreshEffectObjects w = do
   forM_ effects $ renderEffect $ ImageRenderer (imageRsc w) (renderer w)
-  return $
-    filter isAliveEffect $ 
-    map (updateEffect ms) $ 
-    effects ++ checkBirthNewEffect ms
-  where 
+  --liftIO $ print $ length effects
+  let updatedList =
+        filter isAliveEffect $
+        map (updateEffect ms)
+        effects
+  return $ checkBirthNewEffect ms ++ updatedList
+  where
     ms = scene w
     effects = effectObjects ms
 
@@ -24,13 +27,36 @@ refreshEffectObjects w = do
 
 
 checkBirthNewEffect :: MainScene -> [EffectObject]
-checkBirthNewEffect ms = foldr (\e effs -> effs ++ generateEffect e) [] currList
-  where 
+checkBirthNewEffect ms = (checkEffs . checkHarvs) []
+  where
     currList = effectObjects ms
-  --foldr (a -> b -> b) b (t a)
+
+    checkHarvs = \temp -> foldr (\harv effs -> effs ++ checkBirthOvalGen harv) temp $ harvestList $ harvestManager ms
+    checkEffs = \temp -> foldr (\e effs -> effs ++ generateEffect e) temp currList
+
+
+-- 
+checkBirthOvalGen :: Harvest -> [EffectObject]
+checkBirthOvalGen harv = [OvalGen 0 $ toVecF $ installedPos harv | justCropped harv]
 
 
 generateEffect :: EffectObject -> [EffectObject]
+
+ovalGenDuration :: Int
+ovalGenDuration = 2
+ovalGenStepRad :: Int
+ovalGenStepRad = 30
+generateEffect (OvalGen count pos) =
+  let
+    num = count `div` ovalGenDuration
+    r = 4 + num * 2
+    rad = pi * fromIntegral num * fromIntegral ovalGenStepRad / (180 :: Float)
+    delta = Vec (cos rad) (sin rad) ~* fromIntegral r
+    v = Vec (cos rad) (sin rad) ~* 2
+  in [OvalElem 0 (pos ~+ delta) v 
+      | count `mod` ovalGenDuration == 0]
+
+
 generateEffect _ = []
 
 
@@ -44,9 +70,10 @@ updateEffect :: MainScene -> EffectObject -> EffectObject
 updateEffect _ (OvalGen count pos) = OvalGen (count+1) pos
 
 updateEffect _ (OvalElem count pos vel) = OvalElem (count+1) newPos newVel
-  where 
-    newPos = pos
-    newVel = vel
+  where
+    newPos = pos ~+ vel
+    newVel = vel ~+ accel
+    accel = Vec 0 0.1
 
 
 
@@ -56,9 +83,9 @@ updateEffect _ (OvalElem count pos vel) = OvalElem (count+1) newPos newVel
 
 isAliveEffect :: EffectObject -> Bool
 
-isAliveEffect (OvalGen count _) = count > baseFps * 2
+isAliveEffect (OvalGen count _) = count < (360 `div` ovalGenStepRad) * ovalGenDuration
 
-isAliveEffect (OvalElem count _ _) = count > (baseFps * 3) `div` 2
+isAliveEffect (OvalElem count _ _) = count < (baseFps * 3) `div` 2
 
 
 
@@ -70,8 +97,8 @@ isAliveEffect (OvalElem count _ _) = count > (baseFps * 3) `div` 2
 renderEffect :: MonadIO m => ImageRenderer -> EffectObject -> m()
 
 renderEffect (ImageRenderer rsc r) (OvalElem count pos _) = do
-  Rendering.renderPixelartCentral r (oval_16x16 rsc) 
-    (toVecInt pos) $ 
+  Rendering.renderTexture 2 r (oval_16x16 rsc)
+    (toVecInt pos) $
     SrcRect (Vec 0 0) (Vec 16 16)
 
 renderEffect _ _ = return ()
